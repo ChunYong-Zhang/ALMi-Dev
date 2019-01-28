e99e9e3f62dfec81c9e774942a4dff04dd216de7
ChunYong-Zhang <ChunYong-Zhang@foxmail.com>
2019-01-29 00:41:01 +0800

    2019-01-29 00:41:00 Committed

---
+++ microservice-almi-eureka/src/main/java/online/almi/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package online.almi.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/**
+ * WebSecurityConfig
+ * 
+ * @author ChunYong-Zhang
+ *
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().ignoringAntMatchers("/eureka/**");
+    super.configure(http);
+  }
+}
