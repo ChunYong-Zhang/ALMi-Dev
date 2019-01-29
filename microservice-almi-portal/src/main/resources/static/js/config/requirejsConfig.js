6a216bd206a96184aed200970d5842949f6102ae
ChunYong-Zhang <ChunYong-Zhang@foxmail.com>
2019-01-29 21:59:55 +0800

    2019-01-29 21:59:48 Committed

---
+++ microservice-almi-portal/src/main/resources/static/js/config/requirejsConfig.js
@@ -0,0 +1,10 @@
+/*
+ * 通过RequireJS引入公共JS文件
+ */
+require.config({
+	baseUrl : '',
+	paths : {
+		jquery : "/webjars/jquery/3.1.1-1/jquery.min",
+        bootstrap : "/webjars/bootstrap/4.2.1/js/bootstrap.min"
+	}
+})
