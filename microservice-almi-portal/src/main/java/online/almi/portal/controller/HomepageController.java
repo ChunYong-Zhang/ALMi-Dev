d32eddbdf69c4ec016e1da6bdf558450e833ca8f
ChunYong-Zhang <ChunYong-Zhang@foxmail.com>
2019-01-29 23:43:40 +0800

    2019-01-29 23:43:34 Committed

--- microservice-almi-portal/src/main/java/online/almi/portal/controller/HomepageController.java
+++ microservice-almi-portal/src/main/java/online/almi/portal/controller/HomepageController.java
@@ -1,5 +1,6 @@
 package online.almi.portal.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author ChunYong-Zhang
  *
  */
+@Controller
 @RequestMapping("/homepage")
 public class HomepageController {
 	
