de14dde5e2333af6b87b462a5fed614e82c52004
ChunYong-Zhang <ChunYong-Zhang@foxmail.com>
2019-01-30 17:48:55 +0800

    2019-01-30 17:48:41 Committed

---
+++ microservice-almi-portal/src/main/java/online/almi/portal/service/HomepageService.java
@@ -0,0 +1,14 @@
+package online.almi.portal.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * HomepageService
+ * 
+ * @author ChunYong-Zhang
+ *
+ */
+@Service
+public class HomepageService {
+
+}
