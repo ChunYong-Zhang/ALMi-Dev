77d340bddec670f6e06f99c879aeb0deb3fff6b6
ChunYong-Zhang <ChunYong-Zhang@foxmail.com>
2019-02-13 10:48:17 +0800

    2019-02-13 10:48:12 Committed

---
+++ microservice-almi-zuul/src/main/java/online/almi/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package online.almi.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ZuulApplication
+ * 
+ * @author ChunYong-Zhang
+ *
+ */
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
